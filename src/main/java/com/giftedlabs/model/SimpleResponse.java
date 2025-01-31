96dc21f4ad1700735de6917ca10307bb456eccec
Julius <juliussowah@amalitech.com>
2025-01-31 08:51:07 +0000

    feat: Created an entity class to hold my response

---
+++ src/main/java/com/giftedlabs/model/SimpleResponse.java
@@ -0,0 +1,14 @@
+package com.giftedlabs.model;
+
+import lombok.Data;
+
+
+import java.time.LocalDateTime;
+
+@Data
+public class SimpleResponse {
+
+    private String email;
+    private LocalDateTime current_datetime = LocalDateTime.now();
+    private String githubUrl;
+}
