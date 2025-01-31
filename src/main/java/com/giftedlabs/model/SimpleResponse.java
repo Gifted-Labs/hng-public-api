638d351e150219ce113ef9d37297f13907a726aa
Julius <juliussowah@amalitech.com>
2025-01-31 09:30:36 +0000

    feat: Done implementing the endpoint to access the details of the application

--- src/main/java/com/giftedlabs/model/SimpleResponse.java
+++ src/main/java/com/giftedlabs/model/SimpleResponse.java
@@ -3,12 +3,16 @@ package com.giftedlabs.model;
 import lombok.Data;
 
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class SimpleResponse {
 
     private String email;
-    private LocalDateTime current_datetime = LocalDateTime.now();
-    private String githubUrl;
+    private String current_datetime = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT).toString();
+    private String github_url;
 }
